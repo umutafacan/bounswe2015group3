0012976e140486c2284e9313874bb4238266f371
bunyaminn <bunyamin.incee@gmail.com>
2015-05-03 20:57:25 +0300

    addition function was written.

--- Assignment8/src/assignment8/MathematicalOperations.java
+++ Assignment8/src/assignment8/MathematicalOperations.java
@@ -9,4 +9,29 @@ package assignment8;
  */
 public class MathematicalOperations {
 
+	/**
+
+
+
+	 * 2 number addition
+
+	 * @param num1 is the first number
+
+	 * @param num2 is the second number
+
+	 * @return num1 + num2
+
+	 */
+
+
+
+	public int addition(int num1, int num2) {
+
+
+		return num1 + num2 ;
+
+
+
+	}
+
 }
